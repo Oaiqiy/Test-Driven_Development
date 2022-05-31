923224a70ed2cd76b00879f9dfb13c19fb8ac9eb
oaiqiy <1320371940@qq.com>
2022-05-31 21:36:17 +0800

    fix URL

--- suplerlists/urls.py
+++ suplerlists/urls.py
@@ -19,5 +19,5 @@ from lists import urls as list_urls
 
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
-    url(r'^lists/$', include(list_urls))
+    url(r'^lists/', include(list_urls))
 ]
